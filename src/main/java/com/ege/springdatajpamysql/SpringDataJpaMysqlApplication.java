8628d661948d5ddcd3c3ea13efa7e66b0d8ba965
Elga <elga.lika@fti.edu.al>
2024-04-13 11:58:15 +0200

    created spring boot project 2

---
+++ src/main/java/com/ege/springdatajpamysql/SpringDataJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.ege.springdatajpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaMysqlApplication.class, args);
+	}
+
+}
